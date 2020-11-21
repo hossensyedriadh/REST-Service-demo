ecd3978ddbdef5aee6133d6a904661107fb7ecd1
hossensyedriadh <48594108+hossensyedriadh@users.noreply.github.com>
2020-11-22 05:58:00 +0600

    default controller configured

--- src/main/java/live/syedriadh/Demo/REST/Service/controller/DefaultController.java
+++ src/main/java/live/syedriadh/Demo/REST/Service/controller/DefaultController.java
@@ -1,12 +1,16 @@
 package live.syedriadh.Demo.REST.Service.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DefaultController {
+    @Value("${SWAGGER_UI}")
+    private String swaggerUI;
+
     @RequestMapping("/")
     public String redirect() {
-        return "redirect:http://localhost:8080/swagger-ui/";
+        return "redirect:" +swaggerUI;
     }
 }
